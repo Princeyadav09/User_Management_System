6e5f8dec559bbd470b9c56a0c3fb50897b723fd5
Prince yadav <95401115+Princeyadav09@users.noreply.github.com>
2023-09-29 12:10:01 +0530

    Delete src directory

--- src/main/java/com/cafe/management/system/constants/CafeContest.java
+++
@@ -1,6 +0,0 @@
-package com.cafe.management.system.constants;
-
-public class CafeContest {
-    public static final String SOMETHING_WENT_WRONG= "Something Went Wrong.";
-    public static final String INVALID_DATA= "Invalid Data.";
-}
